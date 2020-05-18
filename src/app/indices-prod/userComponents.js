ec2c8e8b3079cab8a516b9c7d6ac0e155ad676ee
Alexander Pustovalov <apustovalov@gmail.com>
2020-05-18 18:01:37 +0300

    Initial commit

---
+++ src/app/indices-prod/userComponents.js
@@ -0,0 +1,2 @@
+
+export default {'usr':{'page':{'PageFrameWithDrawer':require('usr/page/PageFrameWithDrawer.comp').default,},},};
---
+++ src/usr/a_lib/props/margin.props.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+
+export const MarginTypes = {
+  /**
+   * The property sets the margin area.
+   */
+  margin: PropTypes.string,
+  /**
+   * The property sets the margin area on the top of an element.
+   */
+  marginTop: PropTypes.string,
+  /**
+   * The property sets the margin area on the right side of an element.
+   */
+  marginRight: PropTypes.string,
+  /**
+   * The property sets the margin area on the bottom of an element.
+   */
+  marginBottom: PropTypes.string,
+  /**
+   * The property sets the margin area on the left side of an element.
+   */
+  marginLeft: PropTypes.string,
+};
